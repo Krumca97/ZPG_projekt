8025781a748a219ccadc40c5606dedd86cfa06bf
Krumca97 <156618349+Krumca97@users.noreply.github.com>
2025-09-27 13:45:13 +0200

    Add files via upload

---
+++ Projekt_ZPG_LAN0229/shader_program.cpp
@@ -0,0 +1,44 @@
+#include "shader_program.h"
+#include <cstddef>
+
+ShaderProgram::ShaderProgram(const char* vertex_shader, const char* fragment_shader)
+{
+	//vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+	glCompileShader(vertexShader);
+
+	//fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+	glCompileShader(fragmentShader);
+
+	//nastarovani shaderu
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, fragmentShader);
+	glAttachShader(shaderProgram, vertexShader);
+	glLinkProgram(shaderProgram);
+
+	//uklid
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//kontrola
+	GLint status;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
+		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+		delete[] strInfoLog;
+	}
+}
+
+void ShaderProgram::set_shader()
+{
+	glUseProgram(shaderProgram);
+}
+
